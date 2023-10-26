a666ca570763f0e180ae77c2c93085b613a8c818
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-10-26 14:29:59 +0300

    commit

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	fund_management_information_system "fund-management-information-system"
+	_ "fund-management-information-system/docs"
 	"fund-management-information-system/pkg/handler"
 	"fund-management-information-system/pkg/repository"
 	"fund-management-information-system/pkg/repository/postgres"
 	"fund-management-information-system/pkg/service"
+	"github.com/golang-migrate/migrate/v4"
+	migrate_postgres "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/gookit/slog"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"os"
@@ -21,6 +26,29 @@ func initConfig() error {
 	viper.SetConfigName("developer")
 
 	return viper.ReadInConfig()
+}
+func applyMigrations(db *sqlx.DB) error {
+	driver, err := migrate_postgres.WithInstance(db.DB, &migrate_postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://shema/",
+		"postgres", driver)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			// Изменений не было
+			return nil
+		}
+		return err
+	}
+
+	return nil
 }
 
 // @title Invest fund management system
@@ -39,17 +67,21 @@ func main() {
 		f.EnableColor = true
 	})
 
-	/*if err := initConfig(); err != nil {
+	if err := initConfig(); err != nil {
 		slog.Fatalf("Ошибка инициализации конфига")
-	}*/
+	}
 	db, err := postgres.NewPostgresDB(postgres.Config{
-		Host:     "localhost",
-		Port:     "5436",
-		Username: "postgres",
-		DBName:   "postgres",
-		SSLMode:  "disable",
-		Password: "'liderkick123'",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
 	})
+
+	if err := applyMigrations(db); err != nil {
+		slog.Fatalf("Ошибка в миграции", err.Error())
+	}
 
 	if err != nil {
 		slog.Fatalf("Ошибка подключения к БД")
@@ -61,7 +93,7 @@ func main() {
 	srv := new(fund_management_information_system.Server)
 
 	go func() {
-		if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 			slog.Fatalf("ошибка в запуске сервера - %s", err.Error())
 		}
 	}()
--- go.mod
+++ go.mod
@@ -5,18 +5,20 @@ go 1.21
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/gookit/slog v0.5.4
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.9
 	github.com/shopspring/decimal v1.3.1
 	github.com/spf13/viper v1.16.0
-	golang.org/x/exp v0.0.0-20220909182711-5c715a9e8561
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.0
+	github.com/swaggo/swag v1.16.2
+	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.2.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
@@ -34,6 +36,8 @@ require (
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/gookit/goutil v0.6.12 // indirect
 	github.com/gookit/gsr v0.1.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -51,13 +55,11 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/gin-swagger v1.6.0 // indirect
-	github.com/swaggo/swag v1.16.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/arch v0.5.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
@@ -67,6 +69,5 @@ require (
 	golang.org/x/tools v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
